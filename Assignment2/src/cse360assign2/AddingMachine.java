235cffd77c9026c4ae4885516519419fa6f3e9a8
ahard <ahard@DESKTOP-BSQVR7T>
2019-10-03 14:32:09 -0700

    Initial version of project with documentation.

---
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assign2;
+/**
+ * Adam Hardy
+ * CSE360-85141
+ * Assignment 2
+ * 
+ * This class adds or subtracts numbers from the total variable and saves a 
+ * history of transactions
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Default constructor for class, initializing total to 0
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 *Get method for obtaining the value of private variable total
+	 * @return the value of total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds to total
+	 * @param value value to be added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts from total
+	 * @param value value to be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Used to save a history of transactions as a String
+	 */
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
